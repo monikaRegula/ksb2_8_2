626e13a5e30cf7ad5715f801a685332ad214c6b2
monikaRegula <monikaregula97@gmail.com>
2023-06-05 20:27:52 +0200

    Initial commit

---
+++ src/main/java/pl/regula/ksb2_8_2/Ksb282Application.java
@@ -0,0 +1,13 @@
+package pl.regula.ksb2_8_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ksb282Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ksb282Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/regula/ksb2_8_2/Ksb282ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.regula.ksb2_8_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ksb282ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
